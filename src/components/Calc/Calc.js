cfe7dab0ed99151b33837a2df399816d4a2ff02d
Boris <bobthei@yandex.ru>
2023-08-03 16:24:57 +0300

    add: SelhozCoeff fix: percentSymb refact: filters

--- src/components/Calc/Calc.js
+++ src/components/Calc/Calc.js
@@ -2,37 +2,84 @@ import { useEffect, useState } from "react";
 
 import logoInvOcenAnalitic from "../../images/investOcenkaAnalitycCenter.svg"
 
-export default function Calc({ calculated }) {
-  const [numenator, setNumenator] = useState();
-  const [denumenator, setDenumenator] = useState();
-  const [degree, setDegree] = useState(calculated);
+export default function Calc({ calculatedRatio, isSend }) {
+  const [numerator, setNumerator] = useState(undefined);
+  const [denumerator, setDenumerator] = useState(undefined);
+  const [isResult, setIsResult] =  useState(false);  
+  const [result, setResult] =  useState('Нет данных');
+  const [resultPercent, setResultPercent] = useState('Нет данных')
+  const [degree, setDegree] = useState(isSend ? calculatedRatio : '');
 
-  let result = ((denumenator / numenator) ** degree).toFixed(4);
-  let result_percent = ((result - 1) * 100).toFixed(2);
-  let isResult =
-    !isNaN(denumenator) && !isNaN(numenator) && !isNaN(degree) && String(result).includes('.')
-      ? true
-      : false;
+  // let result = degree < 0 ? ((numerator ** degree / denumerator ** degree) ).toFixed(4) : ((denumerator ** degree / numerator ** degree) ).toFixed(4);
+  // degree < 0 ? console.log('Отрицательно') : console.log('Положително');  
+  // let result_percent = ((result - 1) * 100).toFixed(2);
 
-  const handleNumenator = (e) => {
+  // let isResult =
+  //   !isNaN(denumerator) && !isNaN(numerator) && !isNaN(degree) && String(result).includes('.')
+  //     ? true
+  //     : false;
+
+  //  && String(result).includes('.')
+
+  const calcBreakRatio = () => {
+    const res = Math.pow((numerator / denumerator), degree).toFixed(4);
+    let res1;
+    let res2;
+
+    if (denumerator > 0 && numerator > 0 && !isNaN(degree)) {
+      setIsResult(true);
+      // if (degree < 0) {
+      //   console.log('Отрицательная степень'); 
+      //   res = ((Math.pow(numerator, degree) / Math.pow(denumerator, degree)) ).toFixed(4);
+      //   res1 = ((numerator ** degree / denumerator ** degree) ).toFixed(4);
+      //   res2 = Math.pow((numerator / denumerator), degree).toFixed(4);
+      //   console.log('Три вычисления:', res, res1, res2);
+      // } else {
+      //   console.log('Положительная степень');
+      //   res = ((Math.pow(denumerator, degree) / Math.pow(numerator, degree)) ).toFixed(4);
+      //   res1 = ((denumerator ** degree / numerator ** degree) ).toFixed(4);
+      //   res2 = Math.pow((numerator / denumerator), degree).toFixed(4);
+      //   console.log('Три вычисления:', res, res1, res2);
+      // }      
+
+      setResult(res);
+      setResultPercent(((res - 1) * 100).toFixed(2));
+      // console.log('Вычислили:', res);      
+    } else {
+      setIsResult(false);
+      setResult('Нет данных');
+      setResultPercent('Нет данных');
+    }
+  };
+
+  const handleNumerator = (e) => {
     // console.log("Глянь чо", e.target.validity.valid);
+    // console.log('Ща значение числителя:', e.target.value);
     e.preventDefault();
-    setNumenator(e.target.value);
+    setNumerator(e.target.value);
   };
 
   const handleDenumenator = (e) => {
+    // console.log('Ща значение знаменателя:', e.target.value);
     e.preventDefault();
-    setDenumenator(e.target.value);
+    setDenumerator(e.target.value);
   };
 
   const handleDegree = (e) => {
+    // console.log('Щас будет значение степени:', e.target.value);
     e.preventDefault();
     setDegree(e.target.value);
   };
 
   useEffect(() => {
-    setDegree(calculated);
-  }, [calculated]);
+    setDegree(isSend ? calculatedRatio : '');
+  }, [calculatedRatio, isSend]);
+
+  useEffect(() => {
+    // console.log('Значение степени:', degree);
+    // console.log('Слушатель Главного вычисления ');
+    calcBreakRatio();
+  }, [denumerator, numerator, degree, isSend]);
 
   return (
     <form>
@@ -50,8 +97,8 @@ export default function Calc({ calculated }) {
             name="num"
             className="calc__container_input"
             type="number"
-            onChange={handleNumenator}
-            placeholder="Пл. объекта оценки"
+            onChange={handleNumerator}
+            placeholder="Площадь объекта оценки"
           />
         </div>
         <div className="container_item">
@@ -60,7 +107,7 @@ export default function Calc({ calculated }) {
             className="calc__container_input"
             type="number"
             onChange={handleDenumenator}
-            placeholder="Пл. аналога"
+            placeholder="Площадь аналога"
           />
         </div>
         <div className="container_item">
@@ -68,7 +115,7 @@ export default function Calc({ calculated }) {
         </div>
         <div className="container_item">
           <div className="container_degree">
-            <p className="degree_minus">-</p>
+            {/* <p className="degree_minus">-</p> */}
             <input
               name="degr"
               className="calc__container_input calc__container_input_degree"
@@ -82,7 +129,7 @@ export default function Calc({ calculated }) {
         </div>
         <div className="container_item">
           <p className="container_degree-comment container_text">
-            Вводите число без "минуса". <br /> Он уже учтен
+            Коэффициент торможения
           </p>
         </div>
         <div className="container_item">
@@ -91,7 +138,7 @@ export default function Calc({ calculated }) {
         <div className="container_item">
           <div className="comment_result_container">
             <div className="result_part">
-              <p className={isResult ? "result_part_is-result" : ''}>
+              <p className={isResult && "result_part_is-result"}>
                 {isResult ? result : "Нет данных"}
               </p>
             </div>
@@ -99,8 +146,9 @@ export default function Calc({ calculated }) {
             <p className="result_part_or">или</p>
             <div className="result_part">
               <p className={isResult ? "result_part_is-result" : ''}>
-                {isResult ? `${result_percent} %` : "Нет данных"}
-              </p>
+                {isResult ? resultPercent : "Нет данных"}
+                {isResult && (<p className="result_part_is-result result_part_is-result_persent-symb">%</p>)}
+              </p>              
             </div>
           </div>
         </div>
